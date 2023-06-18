c8bf5eddfa7d0676eaf4761a2e359635a4270c69
Victor <arreagav17@gmail.com>
2023-06-18 16:59:16 -0500

    Using desktop entries to get the name of the active window

--- .config/chadwm/window_title.py
+++ .config/chadwm/window_title.py
@@ -1,21 +1,24 @@
-# use xprop to get the window title only NAME
-# title=$(xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) _NET_WM_NAME | cut -d '"' -f 2)
-# IFS='-' read -ra ADDR <<< "$title"
-# title=${ADDR[-1]}
-# echo $title
-
 import subprocess
+import os
 
 NO_WINDOW = '0x0'
 EMPTY_TITLE = ''
+PATH_USR = '/usr/share/applications/'
+PATH_USR_LOCAL = '/home/vfred0/.local/share/applications/'
 
 active_window_id = subprocess.check_output(['xprop', '-root', '32x', '\t$0', '_NET_ACTIVE_WINDOW']).decode().split()[1]
 
 def exists(window_id) -> bool:
     return window_id != NO_WINDOW
+desktop_entry_name = subprocess.check_output(['xprop', '-id', active_window_id, 'WM_CLASS']).decode().split('"')[1]
 
-window_title = subprocess.check_output(['xprop', '-id', active_window_id, 'WM_CLASS']).decode().split('"')[3]
-title_parts = window_title.split('-')
-title = title_parts[-1].strip()    
-title = title[0].lower() + title[1:]
-print(title)
+desktop_entry_path = PATH_USR + desktop_entry_name + '.desktop'
+if not os.path.exists(desktop_entry_path):    
+    desktop_entry_path = PATH_USR_LOCAL + desktop_entry_name + '.desktop'    
+
+with open(desktop_entry_path, 'r') as f:    
+    for line in f:
+        if line.startswith('Name='):
+            window_title = line.split('=')[1].strip()
+            break    
+print(window_title)
