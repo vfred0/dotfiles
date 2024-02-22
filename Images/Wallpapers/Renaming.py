17c83a3f6ae1a623ac6f2f913c1931578c96a5cd
Victor <arreagav17@gmail.com>
2024-02-22 00:11:22 -0500

    Refactored: added `zfill(2)`

--- Images/Wallpapers/Renaming.py
+++ Images/Wallpapers/Renaming.py
@@ -11,19 +11,19 @@ class Renaming:
     def __get_total_wallpapers(self):        
         return len(os.listdir(os.getcwd()))    
 
-    def extract_number(filename):
+    def __extract_number(self, filename):
         match = re.search(r'\d+', filename)
         return int(match.group()) if match else None
 
     def __rename_wallpapers(self):
         files = os.listdir(os.getcwd())
         jpg_files = [f for f in files if f.endswith('.jpg')]
-        jpg_files.sort(key=Renaming.extract_number)
+        jpg_files.sort(key = lambda x: self.__extract_number(x))
         
 
         for i, filename in enumerate(jpg_files, start=1):
             print(f"File: {filename}")            
-            new_name = f'{i}.jpg'
+            new_name = f'{str(i).zfill(2)}.jpg'                         
             os.rename(filename, new_name)
                          
             
